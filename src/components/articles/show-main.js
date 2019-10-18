ee5b11fb24ed87ce152fad87a7fea11b73781482
Paul Gaumer <mitsudomoe8@gmail.com>
2019-10-18 19:10:11 +0200

    Add sanity to react serializer to display articles' body

--- src/components/articles/show-main.js
+++ src/components/articles/show-main.js
@@ -1,9 +1,23 @@
 import React from "react"
+import BaseBlockContent from "@sanity/block-content-to-react"
+
+const serializers = {
+  types: {
+    block(props) {
+      switch (props.node.style) {
+        case "h2":
+          return <h2 className="text-3xl">{props.children}</h2>
+        default:
+          return <p className="mt-6 mb-12">{props.children}</p>
+      }
+    },
+  },
+}
 
 const ShowMain = ({ article }) => {
   return (
-    <div className="mt-10">
-      <p>He</p>
+    <div className="mt-12">
+      <BaseBlockContent blocks={article._rawBody} serializers={serializers} />
     </div>
   )
 }
