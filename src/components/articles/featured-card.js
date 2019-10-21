9252fec3d8f0f3b63796e019fe18739326b9167a
Paul Gaumer <mitsudomoe8@gmail.com>
2019-10-21 10:42:05 +0200

    Fix height issue

--- src/components/articles/featured-card.js
+++ src/components/articles/featured-card.js
@@ -5,42 +5,31 @@ import { Link } from "gatsby"
 const FeaturedCard = ({ article }) => {
   const art = article.node
   return (
-    <div
-      className="flex mt-8 bg-gray-100 border-solid border-0 rounded"
-      style={{ maxHeight: "320px" }}
-    >
-      <div className="w-1/3">
-        <Img
-          fluid={art.mainImage.asset.fluid}
-          alt={`portrait of ${art.interviewee}`}
-          className="h-full object-cover rounded-tl rounded-bl"
-          imgStyle={{ objectPosition: "top" }}
-        />
-      </div>
-      <div className="w-2/3 pl-8 pr-4 py-4">
-        <h3 className="text-2xl text-gray-800 font-semibold">{art.title}</h3>
-        <p className="text-lg text-gray-600">- {art.interviewee}</p>
-        <div className="break-words pt-4">
-          {/* {art._rawBody.map(el => {
-            return <p>{el.children[0].text}</p>
-          })} */}
-          {/* {art._rawBody[1].children[0].text} */}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum...
+    <Link to={`/${art.slug.current}`} className="">
+      <div className="flex items-center mt-8 bg-gray-100 border-solid border-0 rounded">
+        <div className="w-1/3">
+          <Img
+            fluid={art.mainImage.asset.fluid}
+            alt={`portrait of ${art.interviewee}`}
+            className="h-full object-cover rounded-tl rounded-bl"
+            imgStyle={{ objectPosition: "top" }}
+          />
         </div>
-        <Link
-          to={`/${art.slug.current}`}
-          className="text-sm text-gray-600 font-light underline hover:text-orange-400"
-        >
-          Read more...
-        </Link>
+        <div className="w-2/3 pl-8 pr-4 py-4">
+          <h3 className="text-2xl text-gray-800 font-semibold">{art.title}</h3>
+          <p className="text-lg text-gray-600">- {art.interviewee}</p>
+          <div className="break-words pt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum...
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
