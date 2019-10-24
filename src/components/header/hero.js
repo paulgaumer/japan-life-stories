b6fadcfc3728402a1e251027c0dde8e1a07f937f
Paul Gaumer <mitsudomoe8@gmail.com>
2019-10-24 17:41:40 +0200

    sticky menu works

--- src/components/header/hero.js
+++ src/components/header/hero.js
@@ -5,26 +5,24 @@ const Hero = () => {
   return (
     <div className="flex flex-col items-center pt-12 pb-3">
       <Link to="/">
-        <h1 className="">
-          <span className="invisible">Lives in Japan</span>
-          <svg
-            width="100%"
-            height="81"
-            viewBox="0 0 623 81"
-            xmlns="http://www.w3.org/2000/svg"
+        <h1 className="invisible">Japan Life Stories</h1>
+        <svg
+          width="100%"
+          height="81"
+          viewBox="0 0 623 81"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <text
+            fill="#000"
+            fill-rule="evenodd"
+            font-family="NanumBrush, Nanum Brush Script"
+            font-size="100"
           >
-            <text
-              fill="#000"
-              fill-rule="evenodd"
-              font-family="NanumBrush, Nanum Brush Script"
-              font-size="100"
-            >
-              <tspan x="0" y="74">
-                Japan Life Stories
-              </tspan>
-            </text>
-          </svg>
-        </h1>
+            <tspan x="0" y="74">
+              Japan Life Stories
+            </tspan>
+          </text>
+        </svg>
 
         <h2 className="py-6 text-center">
           Inspiring stories from foreigners who built a life in Japan
--- src/components/header/main-menu.js
+++ src/components/header/main-menu.js
@@ -1,21 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Nav = styled.nav`
+  position: sticky;
+  top: 0px;
+  z-index: 2;
+`
 
 const MainMenu = () => {
   return (
-    <nav>
-      <div className="flex justify-center py-4 border border-solid border-black border-b-4 border-l-0 border-r-0">
-        <Link to="/" className="pr-8 hover:text-orange-400">
-          Stories
-        </Link>
-        <Link to="/about" className="pr-8 hover:text-orange-400">
-          About
-        </Link>
-        <Link to="/contact" className="hover:text-orange-400">
-          Contact
-        </Link>
-      </div>
-    </nav>
+    <Nav className="flex justify-center py-4 bg-white border border-solid border-black border-b-4 border-l-0 border-r-0 md:max-w-6xl mx-auto px-8">
+      <Link to="/" className="pr-8 hover:text-orange-400">
+        Stories
+      </Link>
+      <Link to="/about" className="pr-8 hover:text-orange-400">
+        About
+      </Link>
+      <Link to="/contact" className="hover:text-orange-400">
+        Contact
+      </Link>
+    </Nav>
   )
 }
 
