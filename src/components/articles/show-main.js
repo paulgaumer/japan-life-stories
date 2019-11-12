b9701e096d1af89d7e371381b900d6b86479ce5c
Paul Gaumer <mitsudomoe8@gmail.com>
2019-11-12 22:26:13 +0100

    Custom link tag in sanity serializer

--- src/components/articles/show-main.js
+++ src/components/articles/show-main.js
@@ -1,5 +1,5 @@
 import React from "react"
-import BaseBlockContent from "@sanity/block-content-to-react"
+import PortableText from "@sanity/block-content-to-react"
 
 const serializers = {
   types: {
@@ -7,28 +7,32 @@ const serializers = {
       switch (props.node.style) {
         case "h2":
           return <h2 className="text-3xl">{props.children}</h2>
-        // case "a":
-        //   return (
-        //     <a
-        //       href={props.node.markDefs[0].href}
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //       className="text-3xl"
-        //     >
-        //       {props.children}
-        //     </a>
-        //   )
         default:
           return <p className="mt-6 mb-12">{props.children}</p>
       }
     },
+  },
+  marks: {
+    link: ({ children, mark }) =>
+      mark.blank ? (
+        <a
+          href={mark.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-orange-400 underline"
+        >
+          {children}
+        </a>
+      ) : (
+        <a href={mark.href}>{children}</a>
+      ),
   },
 }
 
 const ShowMain = ({ article }) => {
   return (
     <div className="mt-16">
-      <BaseBlockContent blocks={article._rawBody} serializers={serializers} />
+      <PortableText blocks={article._rawBody} serializers={serializers} />
     </div>
   )
 }
