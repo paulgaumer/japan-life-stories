36634a48b2d8a8052f9101493d72602d50cc6c95
Paul Gaumer <mitsudomoe8@gmail.com>
2019-11-12 11:03:51 +0100

    Improve cards responsivness on index

--- src/components/articles/article-card.js
+++ src/components/articles/article-card.js
@@ -4,10 +4,24 @@ import Img from "gatsby-image"
 
 const ArticleCard = ({ article }) => {
   return (
-    <div className="flex h-64 border-2 border-solid border-black pt-8 border-b-0 border-l-0 border-r-0 mb-8">
-      <div className="w-3/4 pr-8">
-        <h3 className="text-2xl text-gray-800">{article.title}</h3>
-        <p className="text-base text-gray-600">- {article.interviewee}</p>
+    <div className="md:flex border-2 border-solid border-black pt-8 border-b-0 border-l-0 border-r-0 mb-8">
+      <div className="md:w-3/5 lg:w-3/4 md:pr-8">
+        <div className="flex justify-between items-center md:block">
+          <div>
+            <h3 className="text-2xl text-gray-800">{article.title}</h3>
+            <p className="text-base text-gray-600">- {article.interviewee}</p>
+          </div>
+          {/* Image displays on Mobile only */}
+          <div className="md:hidden w-1/3">
+            <Img
+              fluid={article.mainImage.asset.fluid}
+              alt={`portrait of ${article.interviewee}`}
+              className="h-24 w-24 object-cover rounded-full"
+              imgStyle={{ objectPosition: "center" }}
+            />
+          </div>
+          {/* .. */}
+        </div>
         <p className="pt-4 break-words">{article.summary}</p>
         <Link
           to={`/${article.slug.current}`}
@@ -16,7 +30,7 @@ const ArticleCard = ({ article }) => {
           Read more...
         </Link>
       </div>
-      <div className="w-1/4">
+      <div className="hidden md:block md:w-2/5 lg:w-1/4">
         <Img
           fluid={article.mainImage.asset.fluid}
           alt={`portrait of ${article.interviewee}`}
--- src/components/articles/featured-card.js
+++ src/components/articles/featured-card.js
@@ -6,16 +6,16 @@ const FeaturedCard = ({ article }) => {
   const art = article.node
   return (
     <Link to={`/${art.slug.current}`} className="">
-      <div className="flex items-center mt-8 bg-gray-100 border-solid border-0 rounded">
-        <div className="w-1/3">
+      <div className="lg:flex items-center mt-8 bg-gray-100 border-solid border-0 rounded">
+        <div className="lg:w-1/3">
           <Img
             fluid={art.mainImage.asset.fluid}
             alt={`portrait of ${art.interviewee}`}
-            className="h-full object-cover rounded-tl rounded-bl"
+            className="h-56 md:h-64 lg:h-full object-cover rounded-tl rounded-bl"
             imgStyle={{ objectPosition: "top" }}
           />
         </div>
-        <div className="w-2/3 pl-8 pr-4 py-4">
+        <div className="lg:w-2/3 pl-8 pr-4 py-4">
           <h3 className="text-2xl text-gray-800 font-semibold">{art.title}</h3>
           <p className="text-lg text-gray-600">- {art.interviewee}</p>
           <div className="break-words pt-4">{art.summary}</div>
