acb55d61d630a41fef13e6ca74a4ad7ff13ab48f
Paul Gaumer <mitsudomoe8@gmail.com>
2019-11-29 23:05:20 +0100

    Fix footer fontawesome

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,6 @@ import Footer from "./footer/footer"
 import "../index.css"
 // This ensures that the icon CSS is loaded immediately before attempting to render icons
 import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core"
-// Prevent fontawesome from dynamically adding its css since we did it manually above
-config.autoAddCss = false
 
 const Layout = ({ children, location }) => {
   const data = useStaticQuery(graphql`
