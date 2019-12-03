01b35989ee65de36df2d8cb5c4493d6c31f912de
Paul Gaumer <mitsudomoe8@gmail.com>
2019-12-03 11:51:34 +0100

    Improved images size and add aria tags

--- src/components/header/hero.js
+++ src/components/header/hero.js
@@ -1,33 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
-// import { Link, graphql, useStaticQuery } from "gatsby"
-// import Img from "gatsby-image"
-import podcastBannerNoCity from "../../images/podcast-banner-no-city.png"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+// import podcastBannerNoCity from "../../images/banner.png"
 // import podcastBanner from "../../images/podcast-banner.png"
 
 const Hero = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     file(relativePath: { eq: "podcast-banner-no-city.png" }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 2000, quality: 90) {
-  //           ...GatsbyImageSharpFluid_withWebp_noBase64
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "banner.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500, quality: 90) {
+            ...GatsbyImageSharpFluid_withWebp_noBase64
+          }
+        }
+      }
+    }
+  `)
 
   return (
     // <div className="flex flex-col items-center">
     <div className="">
       <Link to="/">
-        {/* <Img
+        <Img
           fluid={data.file.childImageSharp.fluid}
           alt="JLS Logo"
           className=""
-        /> */}
-        <img src={podcastBannerNoCity} alt="JLS Logo" className="" />
+        />
+        {/* <img src={podcastBannerNoCity} alt="JLS Logo" className="" /> */}
       </Link>
     </div>
   )
