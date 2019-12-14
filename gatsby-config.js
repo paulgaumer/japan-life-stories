2b9fa9070fd2fb89ef83dcba4750f88f70d0837a
Paul Gaumer <mitsudomoe8@gmail.com>
2019-12-14 12:08:14 +0100

    Change sitemap to exclude legal pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,40 @@ module.exports = {
         tailwind: true,
       },
     },
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: ["/legal/*"],
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+
+          allSitePage {
+            edges {
+              node {
+                path
+              }
+            }
+          }
+      }`,
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          }),
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
